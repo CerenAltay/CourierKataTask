9ad161415c102e77a8b3851c7f40a2e69ef0339b
Ceren <cerenaltay.lsb@gmail.com>
2023-04-06 17:11:55 +0100

    Step1 green one parcel returns delivery cost

--- CourierKata/DeliveryCostCalculator.cs
+++ CourierKata/DeliveryCostCalculator.cs
@@ -4,6 +4,26 @@
     {
         public Delivery CalculateDeliveryCost(Delivery delivery)
         {
+            var totalCost = 0;
+
+            if (delivery.Parcels != null)
+            {
+                foreach (var parcel in delivery.Parcels)
+                {
+                    if (parcel.ParcelHeight < 10 && parcel.ParcelHeight < 10 && parcel.ParcelDepth < 10)
+                    {
+                        parcel.ParcelType = ParcelType.Small;
+                    }
+
+                    if (parcel.ParcelType == ParcelType.Small)
+                    {
+                        parcel.ParcelCost = 3m;
+                    }
+
+                    delivery.TotalCost = totalCost + parcel.ParcelCost;
+                }
+            }
+
             return delivery;
         }
     }
