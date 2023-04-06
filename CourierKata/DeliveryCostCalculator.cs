e207dc08c2aa825b602c49b85a19b9e465260086
Ceren <cerenaltay.lsb@gmail.com>
2023-04-06 17:21:10 +0100

    Step1 refactor-extracted helper methods

--- CourierKata/DeliveryCostCalculator.cs
+++ CourierKata/DeliveryCostCalculator.cs
@@ -2,6 +2,11 @@
 {
     public class DeliveryCostCalculator
     {
+        private const decimal SmallSizeCost = 3m;
+        private const decimal MediumSizeCost = 8m;
+        private const decimal LargeSizeCost = 15m;
+        private const decimal XLSizeCost = 25m;
+
         public Delivery CalculateDeliveryCost(Delivery delivery)
         {
             var totalCost = 0;
@@ -10,45 +15,61 @@
             {
                 foreach (var parcel in delivery.Parcels)
                 {
-                    if (parcel.ParcelHeight < 10 && parcel.ParcelHeight < 10 && parcel.ParcelDepth < 10)
-                    {
-                        parcel.ParcelType = ParcelType.Small;
-                    }
-                    else if (parcel.ParcelHeight < 50 && parcel.ParcelWidth < 50 && parcel.ParcelDepth < 50)
-                    {
-                        parcel.ParcelType = ParcelType.Medium;
-                    }
-                    else if (parcel.ParcelHeight < 100 && parcel.ParcelWidth < 100 && parcel.ParcelDepth < 100)
-                    {
-                        parcel.ParcelType = ParcelType.Large;
-                    }
-                    else if (parcel.ParcelHeight >= 100 || parcel.ParcelWidth >= 100 || parcel.ParcelDepth >= 100)
-                    {
-                        parcel.ParcelType = ParcelType.XL;
-                    }
+                    parcel.ParcelType = DefineParcelType(parcel);
 
-                    if (parcel.ParcelType == ParcelType.Small)
-                    {
-                        parcel.ParcelCost = 3m;
-                    }
-                    if (parcel.ParcelType == ParcelType.Medium)
-                    {
-                        parcel.ParcelCost = 8m;
-                    }
-                    if (parcel.ParcelType == ParcelType.Large)
-                    {
-                        parcel.ParcelCost = 15m;
-                    }
-                    if (parcel.ParcelType == ParcelType.XL)
-                    {
-                        parcel.ParcelCost = 25m;
-                    }
+                    parcel.ParcelCost = GetParcelCost(parcel);
 
                     delivery.TotalCost += totalCost + parcel.ParcelCost;
                 }
             }
 
             return delivery;
+        }
+
+        private static ParcelType DefineParcelType(Parcel parcel)
+        {
+            if (parcel.ParcelHeight < 10 && parcel.ParcelHeight < 10 && parcel.ParcelDepth < 10)
+            {
+                parcel.ParcelType = ParcelType.Small;
+            }
+            else if (parcel.ParcelHeight < 50 && parcel.ParcelWidth < 50 && parcel.ParcelDepth < 50)
+            {
+                parcel.ParcelType = ParcelType.Medium;
+            }
+            else if (parcel.ParcelHeight < 100 && parcel.ParcelWidth < 100 && parcel.ParcelDepth < 100)
+            {
+                parcel.ParcelType = ParcelType.Large;
+            }
+            else if (parcel.ParcelHeight >= 100 || parcel.ParcelWidth >= 100 || parcel.ParcelDepth >= 100)
+            {
+                parcel.ParcelType = ParcelType.XL;
+            }
+
+            return parcel.ParcelType;
+        }
+
+        private static decimal GetParcelCost(Parcel parcel)
+        {
+            switch (parcel.ParcelType)
+            {
+                case ParcelType.Small:
+                    parcel.ParcelCost = SmallSizeCost;
+                    break;
+
+                case ParcelType.Medium:
+                    parcel.ParcelCost = MediumSizeCost;
+                    break;
+
+                case ParcelType.Large:
+                    parcel.ParcelCost = LargeSizeCost;
+                    break;
+
+                case ParcelType.XL:
+                    parcel.ParcelCost = XLSizeCost;
+                    break;
+            }
+
+            return parcel.ParcelCost;
         }
     }
 }
