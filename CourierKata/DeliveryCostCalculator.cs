7c2d9cd93810c43b8d6b7e5be55b628e82780e72
Ceren <cerenaltay.lsb@gmail.com>
2023-04-06 17:17:11 +0100

    Step1 green- test2 parcels return delivery costs

--- CourierKata/DeliveryCostCalculator.cs
+++ CourierKata/DeliveryCostCalculator.cs
@@ -14,13 +14,37 @@
                     {
                         parcel.ParcelType = ParcelType.Small;
                     }
+                    else if (parcel.ParcelHeight < 50 && parcel.ParcelWidth < 50 && parcel.ParcelDepth < 50)
+                    {
+                        parcel.ParcelType = ParcelType.Medium;
+                    }
+                    else if (parcel.ParcelHeight < 100 && parcel.ParcelWidth < 100 && parcel.ParcelDepth < 100)
+                    {
+                        parcel.ParcelType = ParcelType.Large;
+                    }
+                    else if (parcel.ParcelHeight >= 100 || parcel.ParcelWidth >= 100 || parcel.ParcelDepth >= 100)
+                    {
+                        parcel.ParcelType = ParcelType.XL;
+                    }
 
                     if (parcel.ParcelType == ParcelType.Small)
                     {
                         parcel.ParcelCost = 3m;
                     }
+                    if (parcel.ParcelType == ParcelType.Medium)
+                    {
+                        parcel.ParcelCost = 8m;
+                    }
+                    if (parcel.ParcelType == ParcelType.Large)
+                    {
+                        parcel.ParcelCost = 15m;
+                    }
+                    if (parcel.ParcelType == ParcelType.XL)
+                    {
+                        parcel.ParcelCost = 25m;
+                    }
 
-                    delivery.TotalCost = totalCost + parcel.ParcelCost;
+                    delivery.TotalCost += totalCost + parcel.ParcelCost;
                 }
             }
 
